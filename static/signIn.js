08b1c39a33e2998ccf9742ed2cbfe9059479479c
Brandon Sov <brandonsov@gmail.com>
2017-04-08 14:58:12 -0700

    Edited login flow

--- static/signIn.js
+++ static/signIn.js
@@ -1,14 +1,14 @@
-
-
 function onSignIn(googleUser) {
     var id_token = googleUser.getAuthResponse().id_token;
+    var id = document.createElement("id_token");
+    id.setAttribute("value", id_token);
+    console.log(id);
+    document.getElementById("loginForm").submit();
+}
 
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://yourbackend.example.com/tokensignin');
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    console.log('Signed in');
-    xhr.onload = function() {
-      console.log('Signed in as: ' + xhr.responseText);
-    };
-    xhr.send('idtoken=' + id_token);
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
 }
