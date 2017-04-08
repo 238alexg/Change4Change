0d5055fd70970be440e4f6fabddb1e67a935fd3b
Cbrooks12 <this.is.chris12@gmail.com>
2017-04-07 21:17:03 -0700

    initial backend commit

---
+++ CONFIG.py
@@ -0,0 +1,6 @@
+"""
+Edit to fit development or deployment environment.
+"""
+### My local development environment
+#SERVER_NAME = "localhost:5000"
+PORT=5000
--- app.py
+++ app.py
@@ -1,3 +1,41 @@
 # app.py for Change4Change
+import flask
+from flask import request
+from flask import url_for
+from flask import jsonify # For AJAX transactions
+
+import json
+import logging
+
+###
+# Globals
+###
+app = flask.Flask(__name__)
+import CONFIG
+
+import uuid
+app.secret_key = str(uuid.uuid4())
+app.debug = CONFIG.DEBUG
+app.logger.setLevel(logging.DEBUG)
+
+#############
+####Pages####
+#############
+
+### Home Page ###
+
+@app.route("/")
+@app.route("/index")
+def index():
+    app.logger.debug("Main page entry")
+    return flask.render_template('index.html')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    app.logger.debug("Page not found")
+    flask.session['linkback'] = flask.url_for("index")
+    return flask.render_template('page_not_found.html'), 404
+
 
 
